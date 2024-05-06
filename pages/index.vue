1c6f0d4c7fffdf650c77229d9ff7d8c0ff3face2
mctursh <ayoade@seamlesshr.com>
2024-05-06 21:41:21 +0100

    changed contact us position

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="desktop:max-w-full desktop-above:max-w-[1440px] desktop-above:my-0 desktop-above:mx-auto h-[92vh] overflow-hidde">
+    <div class="desktop:max-w-full desktop-above:max-w-[1440px] desktop-above:my-0 desktop-above:mx-auto h-[92vh] phone:h-auto">
         <header class="x-pad py-12 h-[17%]">
             <nav class="flex justify-between items-center">
                 <div class="flex items-center gap-x-12 phablet:flex-col phablet: gap-y-6 phablet:items-start">
@@ -24,12 +24,10 @@
                         </button>
                     </div>
                 </div>
-                <div>
-                    <NuxtLink target="_blank" to="mailto:usedingpay@gmail.com" class="phablet:hidden text-black text-base font-medium" :class="{'text-white': !isTabPersonal}" >Contact us</NuxtLink>
-                </div>
+                
             </nav>
         </header>
-        <main class="x-pad h-[83%]">
+        <main :class="{'bg-secondary': !isTabPersonal}" class="x-pad h-[83%]">
             <section class="w-full h-full relative flex items-start" id="personal">
                 <div class="flex w-[60%] desktop:w-[50%] flex-col gap-y-6 phablet:gap-y-12 mini:gap-y-6 mt-20 ipad:mt-32 phablet:mt-20 iphone:mt-10 phone:w-full phone:items-center">
                     <h1 v-show="isTabPersonal" class="text-6xl font-semibold text-black laptop:text-5xl phone:text-6xl android:text-5xl mini:text-4xl iphone:text-start phone:text-center phablet:leading-[48px]">All your cards in one place. Secure payments with just
@@ -39,7 +37,7 @@
                         your cards securely in one place. Our state of the art technology allows you make instant and
                         secure payments with a simple sound.</h4>
                     <h4 v-show="!isTabPersonal" class="text-white font-normal text-xl laptop:text-lg phone:text-center phone:text-2xl android:text-xl mini:text-lg iphone:text-start">Accept payments effortlessly, enjoy instant withdrawals to your local bank, and manage your team with ease. You literally listen to the sound in inflow.</h4>
-                    <form @submit.prevent="subscribe" class="flex items-center gap-x-3 w-3/4 phablet:flex-col phablet:gap-y-4 phablet:items-stretch iphone:w-full">
+                    <form @submit.prevent="subscribe" class="phone:mb-10 flex items-center gap-x-3 w-3/4 phablet:flex-col phablet:gap-y-4 phablet:items-stretch iphone:w-full">
                         <input v-model="email" class="rounded-[100px] py-4 px-6 grow laptop:py-2 laptop:text-sm phone:text-lg android:text-base" type="email"
                             placeholder="Email Address"
                             :class="{
@@ -71,8 +69,11 @@
         }"
         >
         <div class="desktop:max-w-full desktop-above:max-w-[1440px] desktop-above:my-0 desktop-above:mx-auto">
-            <div class="x-pad">
-                <p class="font-medium text-base" :class="{'text-black': !isTabPersonal, 'text-white': isTabPersonal}">Copyright © DingPay 2024.</p>
+            <div class="x-pad flex justify-between">
+                <p class="font-medium text-base iphone:text-sm mini:text-xs" :class="{'text-black': !isTabPersonal, 'text-white': isTabPersonal}">Copyright © DingPay 2024.</p>
+                <div>
+                    <NuxtLink target="_blank" to="mailto:usedingpay@gmail.com" class="text-black text-base font-medium iphone:text-sm mini:text-xs" :class="{'text-white': isTabPersonal}" >Contact us</NuxtLink>
+                </div>
             </div>
         </div>
     </footer>
